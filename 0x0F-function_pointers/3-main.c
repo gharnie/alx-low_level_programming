b85a2d89d0f30b948cb4498d7a7da1be379f98cf
gharnie <62819684+gharnie@users.noreply.github.com>
2022-04-14 01:34:16 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,73 +1,33 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "3-calc.h"
 
 /**
- * op_add - adds a and b
- * @a: first number
- * @b: second number
+ * main - performs simple operations
+ * @argc: number of arguments passed
+ * @argv: array of pointers to arguments
  *
- * Return: sum of a and b
+ * Return: always 0
  */
-int op_add(int a, int b)
+int main(int argc, char *argv[])
 {
-	return (a + b);
-}
+	int a, b, c;
+	int (*f)(int, int);
 
-/**
- * op_sub - subtracts b from a
- * @a: first number
- * @b: second number
- *
- * Return: difference of a and b
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-
-/**
- * op_mul - multiplies a and b
- * @a: first number
- * @b: second number
- *
- * Return: multiplication of a and b
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
- * op_div - divides a by b
- * @a: first number
- * @b: second number
- *
- * Return: integer division of a by b
- */
-int op_div(int a, int b)
-{
-	if (b == 0)
+	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(98);
 	}
-	return (a / b);
-}
-
-/**
- * op_mod - find the remainder of the division of a by b
- * @a: first number
- * @b: second number
- *
- * Return: remainder of the division of a by b
- */
-int op_mod(int a, int b)
-{
-	if (b == 0)
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (f == NULL)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
-	return (a % b);
+	c = f(a, b);
+	printf("%d\n", c);
+	return (0);
 }
